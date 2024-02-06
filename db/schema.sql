fa9ed762e0a5a78857b8108c761bce686d2695ee
unknown <gabebarraza33@gmail.com>
2024-02-06 00:25:27 -0600

    Creating tables within the schema

---
+++ db/schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS employee_info_db;
+CREATE DATABASE employee_info_db;
+
+USE employee_info_db;
+
+CREATE TABLE DEPARTMENT (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+
+CREATE TABLE ROLE (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+    salary DECIMAL(10, 2) NOT NULL
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES DEPARTMENT(id)
+);
+
+CREATE TABLE EMPLOYEE (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES ROLE(id)
+    manager_id INT,
+    FOREIGN KEY (manager_id) REFERENCES EMPLOYEE(id)
+);
